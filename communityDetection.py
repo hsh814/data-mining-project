e2c5319969300d66cd113e0f75c50310ee6fc453
hsh814 <hsh814@naver.com>
2024-05-14 18:10:41 +0900

    Init

--- communityDetection.py
+++ communityDetection.py
@@ -3,6 +3,8 @@ import community as community_louvain
 import random
 import numpy as np
 import argparse
+import enum
+import queue
 
 from sklearn.metrics.cluster import normalized_mutual_info_score
 
@@ -12,6 +14,79 @@ random_seed = 42
 random.seed(random_seed)
 np.random.seed(random_seed)
 
+class NodeType(enum.Enum):
+    Unknown = 0
+    Core = 1
+    NonMember = 2
+    Hub = 3
+    Outlier = 4
+    
+
+class GraphScan:
+    G: nx.Graph
+    epsilon: float
+    core_threshold: int
+
+    def __init__(self, G: nx.Graph, epsilon: float = 0.5, core_threshold: int = 3):
+        self.count = 0
+        # Update weights
+        self.G = G
+        self.epsilon = epsilon
+        self.core_threshold = core_threshold
+        
+    def similarity(self, a: int, b: int) -> float:
+        a_neighbors = len(self.G.neighbors(a))
+        b_neighbors = len(self.G.neighbors(b))
+        if a_neighbors == 0 or b_neighbors == 0:
+            return 0
+        return len(nx.common_neighbors(self.G, a, b)) / (np.sqrt(a_neighbors * b_neighbors))
+    
+    def get_neighbors(self, node: int) -> Set[int]:
+        return set(self.G.neighbors(node))
+    
+    def epsilon_neighborhood(self, node: int) -> Set[int]:
+        en = set()
+        for n in self.get_neighbors(node):
+            if self.similarity(node, n) >= self.epsilon:
+                en.add(n)
+        return en
+
+    def is_core(self, node: int) -> int:
+        return len(self.epsilon_neighborhood(node)) >= self.core_threshold
+    
+    def direct_reachability(self, core: int, node: int) -> Set[int]:
+        # check node is neighbor of core
+        return G.has_edge(core, node)
+    
+    
+
+    def scan(self, k: int) -> List[List[int]]:
+        clusters = list()
+        for node in self.G.nodes():
+            if self.is_core(node):
+                # Make new cluster
+                cluster = list()
+                q = queue.Queue()
+                for neighbor in self.epsilon_neighborhood(node):
+                    q.put(neighbor)
+                while not q.empty():
+                    current = q.get()
+                    neighbors_of_current = 
+                    if current in cluster:
+                        continue
+                    cluster.append(current)
+                    if self.is_core(current):
+                        for neighbor in self.epsilon_neighborhood(current):
+                            q.put(neighbor)
+                
+                
+        return clusters
+
+    def detect_communities(self) -> List[List[int]]:
+        clusters = self.scan()
+        return clusters
+
+
 # Reading the Network Data
 def load_network(file_path: str) -> nx.Graph:
     G = nx.Graph()
@@ -20,9 +95,9 @@ def load_network(file_path: str) -> nx.Graph:
             parts = line.strip().split()
             if len(parts) == 2:
                 G.add_edge(int(parts[0]), int(parts[1]))
+    for node in G.nodes():
+        G.nodes[node]['type'] = NodeType.Unknown
     return G
-
-
 
 
 # Applying the Louvain Algorithm
@@ -59,6 +134,7 @@ def save_communities_to_file(communities: List[List[int]], file_path: str):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Detect communities in a network.')
     parser.add_argument('--networkFile', '-n', type=str, help='The path of the network file.', default="./data/1-1.dat")
+    parser.add_argument("--method", "-m", type=str, help="The method to use for community detection.", default="scan")
     args = parser.parse_args()
 
     community_file_path = args.networkFile.replace('.dat', '.cmty')
@@ -66,6 +142,11 @@ if __name__ == "__main__":
     G = load_network(args.networkFile)
 
     # Detect communities using Louvain method
-    detected_communities = detect_communities_louvain(G)
+    if args.method == "scan":
+        gs = GraphScan(G)
+        detected_communities = gs.detect_communities()
+    else:
+        # Detect communities using Louvain method
+        detected_communities = detect_communities_louvain(G)
 
     save_communities_to_file(detected_communities, community_file_path)
