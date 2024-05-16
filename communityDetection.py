98601fe9856f418b9d3a13f4929a65357b25f84a
hsh814 <hsh814@naver.com>
2024-05-16 11:35:32 +0900

    Implement communityDetection by scan algorithm

--- communityDetection.py
+++ communityDetection.py
@@ -17,9 +17,10 @@ np.random.seed(random_seed)
 class NodeType(enum.Enum):
     Unknown = 0
     Core = 1
-    NonMember = 2
-    Hub = 3
-    Outlier = 4
+    InCluster = 2
+    NonMember = 3
+    Hub = 4
+    Outlier = 5
     
 
 class GraphScan:
@@ -27,22 +28,23 @@ class GraphScan:
     epsilon: float
     core_threshold: int
 
-    def __init__(self, G: nx.Graph, epsilon: float = 0.5, core_threshold: int = 3):
+    def __init__(self, G: nx.Graph, epsilon: float = 0.25, core_threshold: int = 1):
         self.count = 0
         # Update weights
         self.G = G
         self.epsilon = epsilon
         self.core_threshold = core_threshold
         
-    def similarity(self, a: int, b: int) -> float:
-        a_neighbors = len(self.G.neighbors(a))
-        b_neighbors = len(self.G.neighbors(b))
-        if a_neighbors == 0 or b_neighbors == 0:
-            return 0
-        return len(nx.common_neighbors(self.G, a, b)) / (np.sqrt(a_neighbors * b_neighbors))
-    
     def get_neighbors(self, node: int) -> Set[int]:
         return set(self.G.neighbors(node))
+        
+    def similarity(self, a: int, b: int) -> float:
+        a_neighbors = self.get_neighbors(a)
+        b_neighbors = self.get_neighbors(b)
+        inter = a_neighbors.intersection(b_neighbors)
+        if len(inter) == 0:
+            return 0
+        return (len(inter) + 2) / (np.sqrt((len(a_neighbors) + 1) * len(b_neighbors) + 1))
     
     def epsilon_neighborhood(self, node: int) -> Set[int]:
         en = set()
@@ -52,39 +54,122 @@ class GraphScan:
         return en
 
     def is_core(self, node: int) -> int:
-        return len(self.epsilon_neighborhood(node)) >= self.core_threshold
+        return len(self.epsilon_neighborhood(node)) > self.core_threshold
     
     def direct_reachability(self, core: int, node: int) -> Set[int]:
         # check node is neighbor of core
         return G.has_edge(core, node)
     
+    def has_label(self, node: int) -> bool:
+        return self.G.nodes[node]['type'] != NodeType.Unknown
     
+    def in_cluster(self, node: int) -> bool:
+        nt = self.G.nodes[node]['type']
+        return nt == NodeType.InCluster or nt == NodeType.Core
+    
+    def is_non_member(self, node: int) -> bool:
+        return self.G.nodes[node]['type'] == NodeType.NonMember
+    
+    def add_core(self, node: int, clusters: Dict[int, List[int]], cid: int):
+        clusters[cid] = list()
+        clusters[cid].append(node)
+        self.G.nodes[node]['type'] = NodeType.Core
+        self.G.nodes[node]['cid'] = cid
+    
+    def add_to_cluster(self, node: int, clusters: Dict[int, List[int]], cid: int):
+        clusters[cid].append(node)
+        self.G.nodes[node]['type'] = NodeType.InCluster
+        self.G.nodes[node]['cid'] = cid
+        
+    def mark_as_non_member(self, node: int):
+        self.G.nodes[node]['type'] = NodeType.NonMember
+        
+    def check_hub(self, node: int) -> bool:
+        # Check if node is a hub
+        # A hub is a node that neighbors belong to more than one cluster
+        neighbors = self.get_neighbors(node)
+        belong = set()
+        for n in neighbors:
+            if self.in_cluster(n):
+                belong.add(self.G.nodes[n]['cid'])
+        return len(belong) > 1
+    
+    def assign_to_clusters(self, non_members: Set[int], clusters: Dict[int, List[int]]):
+        for hub in non_members:
+            neighbors = self.get_neighbors(hub)
+            belong = dict()
+            for n in neighbors:
+                if self.in_cluster(n):
+                    cid = self.G.nodes[n]['cid']
+                    if cid in belong:
+                        belong[cid] += 1
+                    else:
+                        belong[self.G.nodes[n]['cid']] = 1
+            # If no neighbors belong to any cluster, create a new cluster
+            if len(belong) == 0:
+                new_cid = len(clusters) + 1
+                self.add_core(hub, clusters, new_cid)
+            # If all neighbors belong to the same cluster, assign to that cluster
+            elif len(belong) == 1:
+                cid = list(belong)[0]
+                self.add_to_cluster(hub, clusters, cid)
+            else:
+                # Assign to the cluster with the most neighbors
+                max_cid = -1
+                max_count = -1
+                for cid in belong:
+                    count = belong[cid]
+                    if count > max_count:
+                        max_count = count
+                        max_cid = cid
+                self.add_to_cluster(hub, clusters, max_cid)
 
-    def scan(self, k: int) -> List[List[int]]:
-        clusters = list()
+    def scan(self) -> Dict[int, List[int]]:
+        cid = 0
+        clusters: Dict[int, List[int]] = dict()
+        non_members = set()
         for node in self.G.nodes():
-            if self.is_core(node):
-                # Make new cluster
-                cluster = list()
+            if self.has_label(node):
+                continue
+            nbrs = self.epsilon_neighborhood(node)
+            # Check if node is core
+            if len(nbrs) > self.core_threshold:
+                cid += 1
+                self.add_core(node, clusters, cid)
                 q = queue.Queue()
-                for neighbor in self.epsilon_neighborhood(node):
-                    q.put(neighbor)
+                for nbr in nbrs:
+                    q.put(nbr)
                 while not q.empty():
                     current = q.get()
-                    neighbors_of_current = 
-                    if current in cluster:
-                        continue
-                    cluster.append(current)
-                    if self.is_core(current):
-                        for neighbor in self.epsilon_neighborhood(current):
-                            q.put(neighbor)
-                
-                
+                    neighbors_of_current = self.epsilon_neighborhood(current)
+                    neighbors_of_current.add(current)
+                    for nc in neighbors_of_current:
+                        if not self.has_label(nc):
+                            q.put(nc)
+                            self.add_to_cluster(nc, clusters, cid)
+                        if self.is_non_member(nc):
+                            self.add_to_cluster(nc, clusters, cid)
+                            non_members.remove(nc)
+            else: # node is not core
+                non_members.add(node)
+                self.mark_as_non_member(node)
+        outliers = list()
+        hubs = list()
+        for n in non_members:
+            if self.check_hub(n):
+                hubs.append(n)
+                self.G.nodes[n]['type'] = NodeType.Hub
+            else:
+                outliers.append(n)
+                self.G.nodes[n]['type'] = NodeType.Outlier
+        # Assign hubs and outliers to clusters
+        print(f"[stat] [clusters {len(clusters)}] [hubs {len(hubs)}] [outliers {len(outliers)}]")
+        self.assign_to_clusters(non_members, clusters)
         return clusters
 
     def detect_communities(self) -> List[List[int]]:
         clusters = self.scan()
-        return clusters
+        return list(clusters.values())
 
 
 # Reading the Network Data
